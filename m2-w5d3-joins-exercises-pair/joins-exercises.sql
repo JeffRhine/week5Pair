76bdd9baf0acd242838835e7f4c354694d244c99
Jeff Rhine <j.s.rhine@gmail.com>
2017-09-28 13:03:02 -0400

    done

--- m2-w5d3-joins-exercises-pair/joins-exercises.sql
+++ m2-w5d3-joins-exercises-pair/joins-exercises.sql
@@ -3,30 +3,66 @@
 
 -- 1. The city name, country name, and city population of all cities in Europe with population greater than 1 million
 -- (36 rows)
+SELECT c.name,ci.name,ci.population
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+WHERE ci.population>1000000 AND c.continent='Europe';
 
 -- 2. The city name, country name, and city population of all cities in countries where French is an official language and the city population is greater than 1 million
 -- (2 rows)
-
+SELECT ci.name, c.name, ci.population
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE cl.language='French' AND ci.population>1000000 AND cl.isofficial=true;
 -- 3. The name of the countries and continents where the language Javanese is spoken
 -- (1 row)
-
+SELECT c.name, c.continent
+FROM country c
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE cl.language='Javanese' ;
 -- 4. The names of all of the countries in Africa that speak French as an official language
 -- (5 row)
-
+SELECT c.name
+FROM country c
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE c.continent='Africa' and cl.language='French' AND  cl.isofficial=true;
 -- 5. The average city population of cities in Europe
 -- (average city population in Europe: 287,684)
-
+SELECT AVG(ci.population)
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+WHERE c.continent='Europe';
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
-
+SELECT AVG(ci.population)
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+WHERE c.continent='Asia';
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
-
+SELECT COUNT(ci.name)
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE cl.language='English' AND cl.isofficial=true;
 -- 8. The average population of cities in countries where the official language is English
 -- (average population of cities where English is official language: 285,809)
-
+SELECT AVG(ci.population)
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE cl.language='English' AND cl.isofficial=true;
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
-
+SELECT c.continent, MAX(ci.population)
+FROM city ci
+JOIN country c ON c.code=ci.countrycode;
+GROUP BY continent;
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+SELECT ci.name, cl.language
+FROM city ci
+JOIN country c ON c.code=ci.countrycode
+JOIN countrylanguage cl ON cl.countrycode=c.code
+WHERE c.continent='South America' AND ci.population>1000000 AND cl.isofficial=true;
--- m2-w5d4-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
+++ m2-w5d4-constraints-and-transactions-exercises-pair/constraints-and-transactions-exercises-pair.sql
@@ -4,31 +4,60 @@
 -- 1. Add Superman's hometown, Smallville, Kansas to the city table. The 
 -- countrycode is 'USA', and population of 45001. (Yes, I looked it up on 
 -- Wikipedia.)
-
+INSERT INTO city (id, name, countrycode,district,population)
+VALUES (6000,'Smallville', 'USA','Kansas',45001);
 -- 2. Add Kryptonese to the countrylanguage table. Kryptonese is spoken by 0.0001
 -- percentage of the 'USA' population.
-
+INSERT INTO countrylanguage (language, countrycode, isofficial, percentage) 
+VALUES ('Krytonese', 'USA', false, .0001);
 -- 3. After heated debate, "Kryptonese" was renamed to "Krypto-babble", change 
 -- the appropriate record accordingly.
-
+UPDATE countrylanguage SET language='Krypto-babble' WHERE language='Krytonese' AND countrycode='USA';
 -- 4. Set the US captial to Smallville, Kansas in the country table.
-
+UPDATE country SET capital=6000 WHERE code='USA';
 -- 5. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
-
+DELETE FROM city WHERE id=6000;
+--won't delete because of foreign key error
 -- 6. Return the US captial to Washington.
-
+UPDATE country SET capital=3813 WHERE code='USA';
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
-
+DELETE FROM city WHERE id=6000;
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
+BEGIN TRANSACTION;
 
+UPDATE countrylanguage
+SET isofficial=NOT isofficial 
+WHERE countrycode IN
+(SELECT countrycode
+FROM countrylanguage cl
+JOIN country c ON c.code=cl.countrycode
+WHERE c.indepyear>=1800 AND c.indepyear<=1972);
+
+ROLLBACK;
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
+BEGIN TRANSACTION;
 
+UPDATE city
+SET population=ROUND(population/1000);
+SELECT * FROM city
+
+COMMIT;
+ROlLBACK;
 -- 10. Assuming a country's surfacearea is expressed in miles, convert it to 
 -- meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+BEGIN TRANSACTION;
+
+UPDATE country
+SET surfacearea=surfacearea*1609.34
+WHERE code IN(
+SELECT c.code 
+FROM country c
+JOIN countrylanguage cl ON c.code=cl.countrycode
+WHERE cl.language='French' AND cl.percentage>20)
